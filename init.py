7be2df52790bd04f1a5bdde4856b526b224912a7
Frederic Meeuwissen <frederic.meeuwissen@gmail.com>
2019-08-04 22:46:01 +0200

    Add initial files

---
+++ init.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import os
+import shutil
+import subprocess
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+CUSTOM_REPO = os.environ.get('BOARDS_URL', None)
+CUSTOM_REPO_DIR = '/usr/share/pinout.custom'
+REPO_DIR = '/usr/share/Pinout.xyz'
+os.chdir(REPO_DIR)
+
+if CUSTOM_REPO is not None:
+    sys.path.append(REPO_DIR)
+    import markjaml
+
+    OVERLAY_DIR = 'src/en/overlay'
+    overlays = [os.path.join(OVERLAY_DIR, f) for f in os.listdir(OVERLAY_DIR)]
+
+    devnull = open(os.devnull, 'w')
+
+    print('Removing boards...')
+    for overlay in overlays:
+        data = markjaml.load(overlay)['data']
+        if data['class'] != 'interface':
+            name = os.path.basename(overlay).split('.', 1)[0]
+            cmd = '{}/draft/unpublish.sh {}'.format(REPO_DIR, name)
+            subprocess.call(cmd, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
+
+    print('Adding boards...')
+    subprocess.call(['git', 'clone', CUSTOM_REPO, CUSTOM_REPO_DIR])
+    shutil.move('draft/boards', 'draft/boards.old')
+    shutil.move('draft/overlay', 'draft/overlay.old')
+    shutil.copytree(os.path.join(CUSTOM_REPO_DIR, 'boards'), 'draft/boards')
+    shutil.copytree(os.path.join(CUSTOM_REPO_DIR, 'overlay'), 'draft/overlay')
+
+    for overlay in os.listdir('draft/overlay'):
+        name = overlay.split('.', 1)[0]
+        cmd = '{}/draft/publish.sh {}'.format(REPO_DIR, name)
+        subprocess.call(cmd, shell=True, stdout=devnull, stderr=subprocess.STDOUT)
+
+subprocess.call(['make', 'serve'])
